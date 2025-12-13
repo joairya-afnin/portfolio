698ad8ca03b0ea9ce6431be0fe08a77e5c474b38
Joairya-Afnin <joairyaafnin194@gmail.com>
2025-12-13 09:29:59 +0600

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".contact").onclick = () => {
+    window.location.href = "mailto:joairyaafnin194@gmail.com";
+};
+
+document.querySelector(".cv").onclick = () => {
+    window.open("cv.pdf");
+};
