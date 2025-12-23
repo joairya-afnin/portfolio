d31ceaa844da9c90598159e6556286d845b16ce6
Joairya-Afnin <joairyaafnin194@gmail.com>
2025-12-24 01:29:28 +0600

    Final portfolio update with all pages

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-document.querySelector(".contact").onclick = () => {
-    window.location.href = "mailto:joairyaafnin194@gmail.com";
-};
+// Contact form
+const form = document.getElementById("contactForm");
+const successMsg = document.getElementById("success-msg");
 
-document.querySelector(".cv").onclick = () => {
-    window.open("cv.pdf");
-};
+if (form) {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+
+        fetch(
+          "https://docs.google.com/forms/d/e/1FAIpQLSfQgQU7lKzU4gbT5J1If8GZF4zRtymoVFmNHa2aX4JcvPCNcA/formResponse",
+          {
+            method: "POST",
+            mode: "no-cors",
+            body: formData
+          }
+        ).then(() => {
+            successMsg.style.display = "block";
+            form.reset();
+        }).catch(() => {
+            alert("Something went wrong. Please try again.");
+        });
+    });
+}
